1249d62a141b2e9337c3a0eb657f6ce5adc896a0
rorywhite200 <125914446+rorywhite200@users.noreply.github.com>
2024-04-06 12:44:13 -0700

    added color to side panel, and improved paddings and headers

--- src/app.py
+++ src/app.py
@@ -36,7 +36,7 @@ all_dest = df['DEST_CITY'].unique()
 
 global_widgets = [
     html.H1('FlightFinder'),
-    html.P('Which airline has the smallest delays for your route?'),
+    html.H5('Which airline has the smallest delays for your route?'),
     html.Br(),
     html.Label('Years'),
     dcc.RangeSlider(
@@ -55,7 +55,8 @@ global_widgets = [
         options=all_origin,
         value='Atlanta, GA',
         multi=False,
-        # placeholder='Select a city...'
+        style={'color': "black"}
+       
     ),
     html.Br(),
     html.Label('Destination'),
@@ -64,7 +65,7 @@ global_widgets = [
         options=all_dest,
         value='Denver, CO',
         multi=False,
-        # placeholder='Select a city...'
+        style={'color': "black"}
     )
 ]
 
@@ -80,38 +81,33 @@ cards = dbc.Row([
     dbc.Col(card_average_delay, md=4)
 ])
 
-mock_data = {
-    'x': [1, 2, 3, 4, 5],
-    'y': [10, 20, 30, 40, 50]
-}
-
 
 graph_avg_delay_by_carrier = html.Div([
-    html.P('Average delay by carrier'),
+    html.H6('Average delay by carrier'),
     dvc.Vega(id='bar', spec={}, style={'width': '100%', 'height': '70%'})
 ])
 # graph_avg_delay_by_carrier = dvc.Vega(id='bar', spec={})
 
 graph_number_unique_flights = html.Div([
-    html.P('Number of unique flights'), 
+    html.H6('Number of unique flights'), 
     dvc.Vega(id='stacked_plot', spec={}, style={'width': '100%', 'height': '70%'})
 ])
 # graph_number_unique_flights = dvc.Vega(id='stacked_plot', spec={})
 
 graph_map = html.Div([
-    html.P('Map'),
+    html.H6('Map'),
     dvc.Vega(id='map_plot', spec={}, style={'width': '100%', 'height': '70%'})
 ])
 
 graph_count_by_delay = html.Div([
-    html.P('Probability of Flight Delays'),
+    html.H6('Probability of Flight Delays'),
     dvc.Vega(id='hist', spec={}, style={'width': '100%', 'height': '70%'})
 ])
 
 
 app.layout = dbc.Container([
     dbc.Row([
-        dbc.Col(global_widgets, md=4, style={'marginTop': 20}),
+        dbc.Col(global_widgets, md=4, style={'padding': 40, 'color': 'white', 'backgroundColor': 'rgb(25, 135, 140)', 'boxShadow': "rgba(0, 0, 0, 0.24) 0px 3px 8px" }),
         dbc.Col([
             dbc.Row([
                 dbc.Col(cards, md=12, style={'marginTop': 20})
@@ -126,7 +122,7 @@ app.layout = dbc.Container([
                     dbc.Row([graph_count_by_delay], style={'flex': '1', "padding": "10px"})
                 ], md=6, style={"display": "flex", "flexDirection": "column", "gap": "10px"})
             ], style={'display': 'flex', 'flexDirection': 'row', 'flex': '1'})
-        ], md=8, style={'display': 'flex', 'flexDirection': 'column', 'height': '100vh'})
+        ], md=8, style={'display': 'flex', 'flexDirection': 'column', 'height': '100vh', "paddingLeft": "40px"})
     ])
 ], fluid=True)
 
