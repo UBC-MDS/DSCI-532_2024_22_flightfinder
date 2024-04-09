5330b91c93102a22875ccc475e80c2b4f5b5b28e
allan8392 <allan83.9.2@hotmail.com>
2024-04-09 14:50:54 -0700

    Index dataframe (#58)
    * implements cards
    
    * merged with main
    
    * cleaned data, deleted duplicate code, added default airports
    
    * added pandas,altiar and dash-vega-components to envrioenment
    
    * added index to dataframe

--- src/app.py
+++ src/app.py
@@ -11,7 +11,6 @@ alt.data_transformers.enable('vegafusion')
 cities = pd.read_csv('data/raw/updated_usa_airports.csv')
 cities_lat_long = cities.set_index('city')[['latitude', 'longitude']].apply(tuple, axis=1).to_dict()
 
-
 # Initiatlize the app
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
@@ -28,6 +27,10 @@ df = pd.read_csv('data/processed/data.gzip', compression='gzip',
 
 all_origin = df['ORIGIN_CITY'].unique()
 all_dest = df['DEST_CITY'].unique()
+
+df['year'] = pd.DatetimeIndex(df['FL_DATE'].to_numpy()).year
+df.set_index(['ORIGIN_CITY', 'DEST_CITY', 'year'], inplace=True)
+
 
 # Layout
 
@@ -167,7 +170,9 @@ def __avg_delay(delay_times: np.ndarray) -> float:
     return 0
 
   
+
 def _plot_stacked(df):
+
     _filtered_df = df.copy()
     _filtered_df['DAY_OF_WEEK'] = pd.to_datetime(_filtered_df['FL_DATE']).dt.day_name().apply(lambda x: x[:3])
     _filtered_df['FLIGHT_ID'] = _filtered_df['AIRLINE'] + '-' + _filtered_df['AIRLINE_CODE']
@@ -333,15 +338,9 @@ def cb(origin_dropdown, dest_dropdown, year_range):
     # temporarily disable multi dest
     if isinstance(dest_dropdown, list):
         dest_dropdown = dest_dropdown[0]
-    msk = ((df['ORIGIN_CITY'] == origin_dropdown)
-           & (df['DEST_CITY'] == dest_dropdown)
-           & (pd.DatetimeIndex(df['FL_DATE'].to_numpy()).year >= year_range[0])
-           & (pd.DatetimeIndex(df['FL_DATE'].to_numpy()).year <= year_range[1]))
 
-    _df = df.loc[msk, :]
-    bar_plot = _plot_bar_plot(_df)
-    hist_plot = _plot_hist_plot(_df)
-
+    _df = df.loc[(origin_dropdown, dest_dropdown, year_range), :].copy()
+    _df.reset_index(inplace=True)
 
     # flights on time
     pct_flights_on_time = __pct_on_time_calc(_df.loc[:, 'ARR_DELAY'].to_numpy())
