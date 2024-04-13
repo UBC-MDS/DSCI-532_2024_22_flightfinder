e116c7006d649c49134bdb338daa4d5ffe043844
rorywhite200 <125914446+rorywhite200@users.noreply.github.com>
2024-04-13 12:49:49 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 import dash_vega_components as dvc
-from data import all_origin, all_dest
+from src.data import all_origin, all_dest
 import callbacks
 
 
