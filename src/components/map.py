9c9c8c6a42f8bae2cc311c845fd9edb4dcbc12b6
rorywhite200 <125914446+rorywhite200@users.noreply.github.com>
2024-04-13 20:25:05 -0700

    feat: change map color and add place labels (#75)

--- src/components/map.py
+++ src/components/map.py
@@ -7,33 +7,31 @@ def plot_map(origin, destination, cities_lat_long):
 
     us_states = alt.topo_feature(data.us_10m.url, 'states')
 
-    if not origin or not destination:
-        us_map = alt.Chart(us_states).mark_geoshape(
-            fill='lightgray',
-            stroke='white'
-        ).properties(
-            width='container',
-            height='container'
-        ).project('albersUsa')
+    us_map = alt.Chart(us_states).mark_geoshape(
+        fill='rgba(25, 135, 140, 0.2)',
+        stroke='white'
+    ).properties(
+        width='container', 
+        height='container',
+    ).project('albersUsa')
 
+    if not origin or not destination:
         return us_map.to_dict()
     
-    # Define origin and destination coordinates
-    origin_city = origin.split(",")[0]
-    if '/' in origin_city:
-        origin_city = origin_city.split("/")[0]
-
-    dest_city = destination.split(",")[0]
-    if '/' in dest_city:
-        dest_city = dest_city.split("/")[0]
+    def process_city(city_name):
+        if '/' in city_name:
+            return city_name.split('/')[0]
+        return city_name.split(',')[0]
+    
+    origin_city = process_city(origin)
+    dest_city = process_city(destination)
 
     origin_lat, origin_long = cities_lat_long[origin_city]
     dest_lat, dest_long = cities_lat_long[dest_city]
 
-
-
     origin = {'city': origin_city, 'latitude': origin_lat, 'longitude': origin_long}
     destination = {'city': dest_city, 'latitude': dest_lat, 'longitude': dest_long}
+
 
     # Create DataFrame for points
     points_df = pd.DataFrame([origin, destination])
@@ -44,31 +42,40 @@ def plot_map(origin, destination, cities_lat_long):
         'longitude': [origin['longitude'], destination['longitude']]
     })
 
-    # Create a map of the US
-    us_map = alt.Chart(us_states).mark_geoshape(
-        fill='lightgray',
-        stroke='white'
-    ).properties(
-        width='container', 
-        height='container',
-    ).project('albersUsa')
-
     # Plot points
     points = alt.Chart(points_df).mark_point(
         filled=True,
-        color='red'
+        color='cyan',
+        size=50,
+        shape='square',
+        stroke='rgba(25, 135, 140)',
+        strokeWidth=1
     ).encode(
         longitude='longitude:Q',
         latitude='latitude:Q',
         tooltip=['city:N']
     )
 
+    labels = alt.Chart(points_df).mark_text(
+        align='left',
+        baseline='middle',
+        dx=7,
+        dy=3,
+        fontWeight='bold',
+        
+    ).encode(
+        longitude='longitude:Q',
+        latitude='latitude:Q',
+        text='city'
+    )
+
     # Plot lines
     lines = alt.Chart(line_df).mark_line(
-        color='blue'
+        color='rgba(25, 135, 140)'
     ).encode(
         longitude='longitude:Q',
         latitude='latitude:Q'
     )
-    chart = (us_map + points + lines).to_dict(format = "vega")
+
+    chart = (us_map + lines + points + labels).to_dict(format = "vega")
     return chart
